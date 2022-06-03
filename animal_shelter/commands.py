333e0546c39a890eba549ee998891ee83f4d0acb
Ziqian Li <liz_q@outlook.com>
2022-06-03 02:00:51 +0200

    add database init and forge commands

---
+++ animal_shelter/commands.py
@@ -0,0 +1,54 @@
+from animal_shelter import app, db
+from animal_shelter.models import User, Animal, Application
+import click
+
+
+@app.cli.command()
+@click.option('--drop', is_flag=True, help='Create after drop.')
+def initdb(drop):
+    """Initialize the database."""
+    db.session.commit()
+    if drop:
+        db.drop_all()
+        click.echo('Deleted database.')
+    
+    db.create_all()
+    click.echo('Initialized database.')
+
+@app.cli.command()
+def forge():
+    """Generate fake data."""
+    db.drop_all()
+    db.create_all()
+
+    user = User(username="user", type=False)
+    user.password_hash = user.set_password('123')
+
+    shelter = User(username="shelter", type=False)
+    shelter.password_hash = shelter.set_password('123')
+
+    db.session.add(user)
+    db.session.add(shelter)
+
+    disease_description = 'Totally healthy'
+    animals = [
+        {'name': 'cat1', 'cat': True, 'breed': 'Abyssinian', 'color': 'yellow', 'weight': 12.00, 'disease': disease_description},
+        {'name': 'cat2', 'cat': True, 'breed': 'American Shorthair', 'color': 'blue', 'weight': 10.00, 'disease': disease_description},
+        {'name': 'cat3', 'cat': True, 'breed': 'Egyptian Mau', 'color': 'white', 'weight': 10.00, 'disease': disease_description},
+        {'name': 'dog1', 'cat': False, 'breed': 'ardedDog', 'color': 'black', 'weight': 20.00, 'disease': disease_description},
+        {'name': 'dog1', 'cat': False, 'breed': 'ardedDog', 'color': 'black', 'weight': 20.00, 'disease': disease_description},
+        {'name': 'dog2', 'cat': False, 'breed': 'American Water Spaniel', 'color': 'white', 'weight': 20.00, 'disease': disease_description},
+        {'name': 'dog3', 'cat': False, 'breed': 'Samoyed', 'color': 'white', 'weight': 20.00, 'disease': disease_description},
+    ]
+
+    for a in animals:
+        animal = Animal(name=a['name'],cat=a['cat'], breed=a['breed'], color=a['color'], weight=a['weight'], disease=a['disease'])
+        db.session.add(animal)
+    
+    db.session.commit()
+    click.echo('Done.')
+
+
+
+
+
---
+++ animal_shelter/models.py
@@ -0,0 +1,31 @@
+from animal_shelter import db
+from werkzeug.security import generate_password_hash, check_password_hash
+from flask_login import UserMixin
+
+class User(db.Model, UserMixin):  
+    id = db.Column(db.Integer, primary_key=True)  
+    username = db.Column(db.String(20))
+    password_hash = db.Column(db.String(128))
+    type = db.Column(db.Boolean())
+
+    def set_password(self, password):
+        self.password_hash = generate_password_hash(password)
+
+    def validate_password(self, password):
+        return check_password_hash(self.password_hash, password)
+
+class Animal(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(20))
+    cat = db.Column(db.Boolean()) #True for cat, False for dog
+    breed = db.Column(db.String(50))
+    color = db.Column(db.String(20))
+    weight = db.Column(db.Float())
+    disease = db.Column(db.String(100))
+    
+class Application(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.Integer)
+    animal_id = db.Column(db.Integer)
+    date = db.Column(db.Date())
+    state = db.Column(db.String(50))
